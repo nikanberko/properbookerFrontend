31e6ca4d1b182b857db3e957c4320d45203e8dda
Nikola Pavkovic <nikola.pavkovic@infobip.com>
2023-10-21 21:24:44 +0200

    added api key placeholder

--- app/utils/apiUrls.ts
+++ app/utils/apiUrls.ts
@@ -1,4 +1,4 @@
 export const baseUrl = 'https://f697-31-217-30-28.ngrok.io';
 export const googleVisionUrl = 'https://vision.googleapis.com/v1/images:annotate?key=';
 
-export const googleVisionApiKey = '';
+export const googleVisionApiKey = 'enter your api key';
